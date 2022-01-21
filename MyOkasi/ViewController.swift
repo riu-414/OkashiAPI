af2418fe960fd517c2909f2617f4db615f9f7539
riu-414 <mande_e@ezweb.ne.jp>
2022-01-22 04:22:28 +0900

    Initial Commit

---
+++ MyOkasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyOkasi
+//
+//  Created by 大杉祐弥 on 2022/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
